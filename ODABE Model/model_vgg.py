6e0fc858904d12827d65132b8b5dbff3a1c6e5ed
rghangha <rghangha@asu.edu>
2024-02-21 00:26:34 -0700

    Model VGG

---
+++ ODABE Model/model_vgg.py
@@ -0,0 +1,31 @@
+import math
+
+import torch.nn as nn
+from torchvision.models.vgg import make_layers, cfg
+
+
+class NetVgg(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.extractor = make_layers(cfg=cfg['A'], batch_norm=True)
+        self.regressor = nn.Sequential(
+            nn.Linear(25088, 4096),
+            nn.ReLU(True),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Linear(4096, 2),
+            nn.Sigmoid()
+        )
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
