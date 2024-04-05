98ae6d37f97f73d4285baf740647a8766009375e
ASU ZIL <peng86david@gmail.com>
2024-04-04 17:27:25 -0700

    Train.py

--- train.py
+++ train.py
@@ -2,15 +2,17 @@ from GazeRefineNet import GRN
 import pytorch_lightning as pl
 
 if __name__ == '__main__':
-    # image_dir = r'C:\\Rushi\\ProDataset\\train\\images\\iPhone 5S\\cropped_eyes'
     
-    image_dir = r'C:\Users\ASU Zoom 01\Downloads\ProDataset\train\images\iPhone 5S\cropped_eyes'
-    meta_dir = r'C:\Users\ASU Zoom 01\Downloads\ProDataset\train\meta'
-    save_dir = "../Result"
- 
-    model = GRN(data_path= image_dir, save_path= save_dir)
+    # image_dir = r'C:\Users\rpatil29\ProDataset\train\iPhone 5S\cropped_eyes'
+    # meta_dir = r'C:\Users\rpatil29\ProDataset\train\meta'
+    # save_dir = "../Result"
 
-    trainer = pl.Trainer(accelerator= "cpu", max_epochs= 1, default_root_dir= save_dir, enable_progress_bar= True)
+    dataset_dir = '../ProDataset/'
+    save_dir = '../Result/'
+ 
+    model = GRN(data_path= dataset_dir, save_path= save_dir)
+
+    trainer = pl.Trainer(accelerator= "gpu", max_epochs= 5, default_root_dir= save_dir, enable_progress_bar= True)
     trainer.fit(model)
 
     print("Done")
